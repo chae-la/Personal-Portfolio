c590046e8490ec5db159ddf61334b4bceb17f47f
Michaela <michaelachan45@gmail.com>
2024-04-08 10:34:23 +0100

    Updated with completed projects so far

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-    const hamburgerMenu = document.querySelector(".menu__hamburger");
-    const navigationMenu = document.querySelector(".menu__nav");
+  const hamburgerMenu = document.querySelector(".menu__hamburger");
+  const navigationMenu = document.querySelector(".menu__nav");
 
-    hamburgerMenu.addEventListener("click", () => {
-      navigationMenu.classList.toggle("active");
-    });
+  hamburgerMenu.addEventListener("click", () => {
+    navigationMenu.classList.toggle("active");
   });
+});
 
-
-
-  const greetingOptions = ["Hi!", "Hola!", "Selamat Datang!", "你好!", "안녕하세요!", "こんにちは!"];
+const greetingOptions = [
+  "Hi!",
+  "Hola!",
+  "Selamat Datang!",
+  "你好!",
+  "안녕하세요!",
+  "こんにちは!",
+];
 const speed = 50;
 const repeatDelay = 2000;
 const constantPart = "I'm Michaela";
@@ -18,24 +22,24 @@ const constantPart = "I'm Michaela";
 const typingElement = document.getElementById("typing-animation");
 let currentGreetingIndex = 0;
 
-function typeWriter() {
-const greeting = greetingOptions[currentGreetingIndex];
-const text = `${greeting} ${constantPart}`;
-let i = 0;
-typingElement.innerHTML = "";
+const typeWriter = () => {
+  const greeting = greetingOptions[currentGreetingIndex];
+  const text = `${greeting} ${constantPart}`;
+  let i = 0;
+  typingElement.innerHTML = "";
 
-function typeNextCharacter() {
-if (i < text.length) {
-  typingElement.innerHTML += text.charAt(i);
-  i++;
-  setTimeout(typeNextCharacter, speed);
-} else {
-  setTimeout(typeWriter, repeatDelay);
-}
-}
-typeNextCharacter();
+  function typeNextCharacter() {
+    if (i < text.length) {
+      typingElement.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeNextCharacter, speed);
+    } else {
+      setTimeout(typeWriter, repeatDelay);
+    }
+  }
+  typeNextCharacter();
 
-currentGreetingIndex = (currentGreetingIndex + 1) % greetingOptions.length;
-}
+  currentGreetingIndex = (currentGreetingIndex + 1) % greetingOptions.length;
+};
 
 typeWriter();
